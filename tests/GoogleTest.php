e450ccb6f7c91cf3adcdfdc19e7e086b4a6991ac
Pablo de la Peña <p@shedcollective.org>
2015-12-23 13:39:24 +0000

    Initial commit

---
+++ tests/GoogleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nails\GeoCode\Driver;
+
+class GoogleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFoo()
+    {
+        $this->assertCount(0, array());
+    }
+}
